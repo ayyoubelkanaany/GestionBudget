75b112f13202b5cf077203eb6f5d31f8216e74dd
ayyoub <elkanaany.21@gmail.com>
2020-12-10 16:54:14 +0100

    first-commit

---
+++ src/app/routes/routing/routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+
+
+export const routes: Routes = [
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
+})
+export class RoutingModule { }
